47ead3ebf5d176793594679f235ff64c26d04824
Laurence kite <laurencekite@Laurences-MacBook-Pro.local>
2016-07-06 12:57:49 +0100

    added test for exercise 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,7 @@ describe DockingStation do
   it 'responds to the method "release_bike"' do
     expect(subject).to respond_to(:release_bike)
   end
-
+  it 'Gets a working bike' do
+    expect(subject.release_bike).to be_working
+  end
 end
